f0e74ede3844012521c6199b820f5a6166883c3b
Fahad Alamgir <fahadalamgir777@gmail.com>
2021-02-14 13:33:25 +0200

    This is latest

---
+++ Test/CommandAPI.Tests/CommandTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using CommandAPI.Models;
+using Xunit;
+
+namespace CommandAPI.Tests
+{
+   public class CommandTests:IDisposable
+    {
+        Command testCommand;
+
+        public CommandTests()
+        {
+            testCommand = new Command
+            {
+                HowTo = "Do Something",
+                Platform = "Some Platform",
+                CommandLine = "Some CommandLine"
+            };
+        }
+
+      public void Dispose()
+        {
+            testCommand = null;
+        }
+
+
+        [Fact]
+        public void CanChangeHowTo()
+        {
+            //Arrange
+
+            //Act
+            testCommand.HowTo = "Execute Unit Tests";
+
+            //Assert
+            Assert.Equal("Execute Unit Tests", testCommand.HowTo);
+        }
+
+        [Fact]
+        public void CanChangePlatform()
+        {
+            //Arrange
+
+            //Act
+            testCommand.Platform = ".NET Core";
+
+            //Assert
+            Assert.Equal(".NET Core", testCommand.Platform);
+        }
+
+
+        [Fact]
+
+        public void CanChangeCommandLine()
+        {
+            //Arrange
+
+            //Act
+
+            testCommand.CommandLine = ".NET CLI";
+
+            //Assert
+            Assert.Equal(".NET CLI", testCommand.CommandLine);
+        
+        }
+    }
+}
---
+++ src/CommandAPI/Profiles/CommandsProfile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CommandAPI.Dtos;
+using CommandAPI.Models;
+
+namespace CommandAPI.Profiles
+{
+    public class CommandsProfile:Profile
+    {
+        public CommandsProfile()
+        {
+            //GET
+            CreateMap<Command, CommandReadDto>();
+            
+            //POST
+            CreateMap<CommandCreateDto, Command>();
+            
+            //PUT
+            CreateMap<CommandUpdateDto, Command>();
+
+            //Patch
+            CreateMap<Command, CommandUpdateDto>();
+        }
+
+
+    }
+}
