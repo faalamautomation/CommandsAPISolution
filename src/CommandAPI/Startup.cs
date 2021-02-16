93e36d7d2deff63ac27d1561b9b0d22313ce8e64
Fahad Alamgir <fahadalamgir777@gmail.com>
2021-02-16 15:41:46 +0200

    The commit for setting database in azure through startup.cs

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -53,8 +53,9 @@ namespace CommandAPI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,CommandContext Context)
         {
+            Context.Database.Migrate();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
